42de1a662d7e6ca554de299210d28a4db5bab513
Spark <sparkismad@gmail.com>
2023-12-24 23:14:30 +0100

    Setup options in main class constructor

--- Source/FirstPersonShooter/MonsterShooterCharacter.cpp
+++ Source/FirstPersonShooter/MonsterShooterCharacter.cpp
@@ -3,12 +3,45 @@
 
 #include "MonsterShooterCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
 // Sets default values
 AMonsterShooterCharacter::AMonsterShooterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(40.0f, 90.0f);
+
+	TurnRate = 45.0f;
+	LookUpRate = 45.0f;
+
+	FirstPersonCamera = CreateAbstractDefaultSubobject<UCameraComponent>(TEXT("First Person Camera"));
+	FirstPersonCamera->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCamera->AddRelativeLocation(FVector(-39.65f, 1.75f, 64.0f));
+	FirstPersonCamera->bUsePawnControlRotation = true;
+
+	HandsMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Character Mesh"));
+	HandsMesh->SetOnlyOwnerSee(true);
+	HandsMesh->SetupAttachment(FirstPersonCamera);
+	HandsMesh->bCastDynamicShadow = false;
+	HandsMesh->CastShadow = false;
+	HandsMesh->AddRelativeRotation(FRotator(1.9f, -19.19f, 5.2));
+	HandsMesh->AddRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
+
+	GunMesh = CreateAbstractDefaultSubobject<USkeletalMeshComponent>(TEXT("Gun"));
+	GunMesh->SetOnlyOwnerSee(true);
+	GunMesh->bCastDynamicShadow = false;
+	GunMesh->CastShadow = false;
+	
+	MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("Muzzle Location"));
+	MuzzleLocation->SetupAttachment(GunMesh);
+	MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+
+	GunOffset = FVector(100.0f, 0.0f, 10.0f);
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +63,25 @@ void AMonsterShooterCharacter::SetupPlayerInputComponent(UInputComponent* Player
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AMonsterShooterCharacter::OnFire()
+{
+}
+
+void AMonsterShooterCharacter::MoveForward(float Value)
+{
+}
+
+void AMonsterShooterCharacter::MoveRight(float Value)
+{
+}
+
+void AMonsterShooterCharacter::TurnAtRate(float Rate)
+{
+}
+
+void AMonsterShooterCharacter::LookAtRate(float Rate)
+{
 }
 
--- Source/FirstPersonShooter/MonsterShooterCharacter.h
+++ Source/FirstPersonShooter/MonsterShooterCharacter.h
@@ -26,4 +26,34 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+public:
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* HandsMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* GunMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USceneComponent* MuzzleLocation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = camera)
+		class UCameraComponent* FirstPersonCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = camera)
+		float TurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = camera)
+		float LookUpRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FVector GunOffset;
+
+protected:
+	void OnFire();
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void TurnAtRate(float Rate);
+	void LookAtRate(float Rate);
 };
